be9cd473f000b974f360bcbd73156f9bf568ca5d
Marta M <marmacan.upv@gmail.com>
2023-05-28 10:05:32 +0200

    variables entorno BD: conexión con clever cloud

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const { createPool } = require('mysql2/promise')
 
 const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'app_rutas2'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
 })
 
 module.exports = pool
